2907b9328a65e1644d2fa2c41bb886a4245c1ed9
borgbean <borgbean@sammich.ino>
2013-10-28 21:28:16 -0400

    List views
    add list views

---
+++ src/ISIS/gui/customer/ListTransactions.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ISIS.gui.customer;
+
+import ISIS.gui.ListView;
+
+/**
+ *
+ * @author michaelm
+ */
+public class ListTransactions extends ListView {
+}
---
+++ src/ISIS/gui/item/ListItems.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ISIS.gui.item;
+
+/**
+ *
+ * @author michaelm
+ */
+public class ListItems {
+    
+}
