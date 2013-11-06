0c6cdeecc3320eef2c8904d4b9c779c95c39de6f
erkalicious <erkalicious@icloud.com>
2013-11-06 14:51:59 -0500

    Formatting and etcetera

--- src/ISIS/gui/ErrorLogger.java
+++ src/ISIS/gui/ErrorLogger.java
@@ -1,7 +1,5 @@
 package ISIS.gui;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Level;
@@ -10,38 +8,38 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * 
  */
 public class ErrorLogger {
-
-    private static boolean debug = true;
-
-    public static void error(String error, boolean severe, boolean show) {
-        new ErrorLogger(error, severe, show);
-    }
-
-    public static void error(Exception e, String error, boolean severe,
-            boolean show) {
-        if (debug) {
-            StringWriter excBuffer = new StringWriter();
-            e.printStackTrace(new PrintWriter(excBuffer));
-            e.printStackTrace();
-            new ErrorLogger(error + "\n" + e.getMessage() + "\nStack trace:\n"
-                    + excBuffer.toString(), severe, show);
-        } else {
-            new ErrorLogger(error + "\n" + e.getMessage(), severe, show);
-        }
-    }
-
-    private ErrorLogger(String error, boolean severe, boolean show) {
-        if (show) {
-            JOptionPane.showMessageDialog(null, error, "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-        log(error, severe ? Level.SEVERE : Level.INFO);
-    }
-
-    private static void log(String message, Level level) {
-        Logger.getLogger("ISIS").log(level, message);
-    }
+	
+	private static boolean	debug	= true;
+	
+	public static void error(String error, boolean severe, boolean show) {
+		new ErrorLogger(error, severe, show);
+	}
+	
+	public static void error(Exception e, String error, boolean severe,
+			boolean show) {
+		if (debug) {
+			StringWriter excBuffer = new StringWriter();
+			e.printStackTrace(new PrintWriter(excBuffer));
+			e.printStackTrace();
+			new ErrorLogger(error + "\n" + e.getMessage() + "\nStack trace:\n"
+					+ excBuffer.toString(), severe, show);
+		} else {
+			new ErrorLogger(error + "\n" + e.getMessage(), severe, show);
+		}
+	}
+	
+	private ErrorLogger(String error, boolean severe, boolean show) {
+		if (show) {
+			JOptionPane.showMessageDialog(null, error, "Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+		log(error, severe ? Level.SEVERE : Level.INFO);
+	}
+	
+	private static void log(String message, Level level) {
+		Logger.getLogger("ISIS").log(level, message);
+	}
 }
