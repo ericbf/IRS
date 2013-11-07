1ae2d724ca3c497f220b1068d028213ec096ba58
erkalicious <erkalicious@icloud.com>
2013-11-07 13:22:15 -0500

    Slight improvement to up down

--- src/ISIS/gui/ListView.java
+++ src/ISIS/gui/ListView.java
@@ -3,11 +3,15 @@ package ISIS.gui;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.DefaultFocusTraversalPolicy;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
@@ -36,6 +40,7 @@ public abstract class ListView<E extends Record> extends View {
 				ListView.this.fillTable();
 			}
 		});
+		
 		this.searchField.addKeyListener(new KeyListener() {
 			private final int	META		= 157, BACKSPACE = 8, DOWN = 40,
 					UP = 38;
@@ -159,4 +164,66 @@ public abstract class ListView<E extends Record> extends View {
 	 * Fills the table.
 	 */
 	protected abstract void fillTable();
+	
+	public class HintField extends JTextField {
+		private static final long	serialVersionUID	= 1L;
+		private boolean				hintShown;
+		private boolean				showHint;
+		private String				hint;
+		
+		public HintField() {
+			this("");
+		}
+		
+		public HintField(String hint) {
+			super(hint);
+			this.hint = hint;
+			this.hintShown = true;
+			this.showHint = true;
+			this.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "none");
+			this.getInputMap().put(KeyStroke.getKeyStroke("UP"), "none");
+			this.getInputMap().put(
+					KeyStroke.getKeyStroke(KeyEvent.VK_DOWN,
+							java.awt.event.InputEvent.META_DOWN_MASK), "none");
+			this.getInputMap().put(
+					KeyStroke.getKeyStroke(KeyEvent.VK_UP,
+							java.awt.event.InputEvent.META_DOWN_MASK), "none");
+			this.addFocusListener(this.new Listener());
+		}
+		
+		public void setShowHint(boolean b) {
+			this.showHint = b;
+		}
+		
+		@Override
+		public String getText() {
+			if (!this.hintShown) return super.getText();
+			else return "";
+		}
+		
+		class Listener implements FocusListener {
+			
+			@Override
+			public void focusGained(FocusEvent fe) {
+				HintField.this.selectAll();
+				if (HintField.this.hintShown) {
+					HintField.this.setText("");
+				}
+				HintField.this.hintShown = false;
+				ListView.this.table.setFocusable(false);
+			}
+			
+			@Override
+			public void focusLost(FocusEvent fe) {
+				HintField.this.setCaretPosition(0);
+				if (HintField.this.getText().length() == 0) {
+					HintField.this.hintShown = true;
+					if (HintField.this.showHint) {
+						HintField.this.setText(HintField.this.hint);
+					}
+				}
+				ListView.this.table.setFocusable(true);
+			}
+		}
+	}
 }
