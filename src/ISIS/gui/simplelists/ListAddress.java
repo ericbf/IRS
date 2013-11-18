205fe9755f507978a29ea5725ff343f48e39a88c
erkalicious <erkalicious@icloud.com>
2013-11-17 21:00:47 -0500

    blah

--- src/ISIS/gui/simplelists/ListAddress.java
+++ src/ISIS/gui/simplelists/ListAddress.java
@@ -1,47 +1,53 @@
 package ISIS.gui.simplelists;
 
-import ISIS.customer.Customer;
-import ISIS.database.DB;
-import ISIS.database.Field;
-import ISIS.database.Record;
-import ISIS.gui.*;
-import ISIS.gui.address.AddEditAddress;
-import ISIS.misc.Address;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+
+import ISIS.customer.Customer;
+import ISIS.database.DB;
+import ISIS.database.Field;
+import ISIS.database.Record;
+import ISIS.gui.ErrorLogger;
+import ISIS.gui.IRSTableModel;
+import ISIS.gui.SimpleListView;
+import ISIS.gui.SplitPane;
+import ISIS.gui.View;
+import ISIS.gui.address.AddEditAddress;
+import ISIS.misc.Address;
+
 /**
  * This should NEVER be pushed, only embedded.
  */
 public class ListAddress extends SimpleListView<Address> {
 	private static final long	serialVersionUID	= 1L;
-    private final Customer customer;
-
-	public ListAddress(SplitPane splitPane, View pusher, Customer customer, Integer key,
-			boolean selectMode) {
+	private final Customer		customer;
+	
+	public ListAddress(SplitPane splitPane, View pusher, Customer customer,
+			Integer key, boolean selectMode) {
 		super(splitPane, pusher, false, "SELECT a.* FROM address AS a left "
 				+ "join customer_address AS ca ON a.pkey=ca.address WHERE "
 				+ "ca.customer=?", key);
-
+		
 		this.customer = customer;
 		this.setTableModel(new IRSTableModel() {
 			private static final long	serialVersionUID	= 1L;
-
+			
 			@Override
 			public void addRow(Record record) {
 				Address address = (Address) record;
 				Object[] array = new Object[this.getColumnCount()];
 				int i = 0;
-
+				
 				array[i++] = address.getTitle();
 				array[i++] = address.getStreetAddress(); // TODO: flesh this out
-
+				
 				super.addRow(array);
 				ListAddress.this.keys.add(address.getPkey());
 			}
@@ -50,8 +56,9 @@ public class ListAddress extends SimpleListView<Address> {
 		int x = 0;
 		int y = 0;
 		GridBagConstraints c = new GridBagConstraints();
-
+		
 		if (selectMode) { // we're selecting an address.
+			@SuppressWarnings("unused")
 			JButton select;
 			// TODO: THIS
 		} else { // we're adding/removing addresses.
@@ -60,7 +67,8 @@ public class ListAddress extends SimpleListView<Address> {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					ListAddress.this.splitPane.push(new AddEditAddress(
-							ListAddress.this.splitPane, ListAddress.this.customer),
+							ListAddress.this.splitPane,
+							ListAddress.this.customer),
 							SplitPane.LayoutType.HORIZONTAL,
 							ListAddress.this.pusher);
 				}
@@ -72,23 +80,25 @@ public class ListAddress extends SimpleListView<Address> {
 			c.gridx = x = 0;
 			c.weightx = 1;
 			this.add(addButton, c);
-            JButton deleteButton = new JButton("Baleete");
+			JButton deleteButton = new JButton("Baleete");
 			deleteButton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-                    int selected = ListAddress.this.table.getSelectedRow();
-                    if (selected == -1) {
-                        return;
-                    }
-
-                    int pkey = ListAddress.this.keys.get(selected);
-                    try {
-                        ListAddress.this.customer.removeAddress(new Address(pkey, true));
-                        ListAddress.this.customer.save();
-                    } catch (SQLException ex) {
-                        ErrorLogger.error(ex, "Failed to delete address record.", true, true);
-                    }
-                    ListAddress.this.fillTable();
+					int selected = ListAddress.this.table.getSelectedRow();
+					if (selected == -1) {
+						return;
+					}
+					
+					int pkey = ListAddress.this.keys.get(selected);
+					try {
+						ListAddress.this.customer.removeAddress(new Address(
+								pkey, true));
+						ListAddress.this.customer.save();
+					} catch (SQLException ex) {
+						ErrorLogger.error(ex,
+								"Failed to delete address record.", true, true);
+					}
+					ListAddress.this.fillTable();
 				}
 			});
 			c = new GridBagConstraints();
@@ -98,26 +108,28 @@ public class ListAddress extends SimpleListView<Address> {
 			c.gridx = x = 0;
 			c.weightx = 1;
 			this.add(deleteButton, c);
-            JButton viewButton = new JButton("View");
+			JButton viewButton = new JButton("View");
 			viewButton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-                    int selected = ListAddress.this.table.getSelectedRow();
-                    if (selected == -1) {
-                        return;
-                    }
-
-                    int pkey = ListAddress.this.keys.get(selected);
-                    try {
-                        ListAddress.this.splitPane.push(new AddEditAddress(
-                                ListAddress.this.splitPane, ListAddress.this.customer, pkey),
-                                                        SplitPane.LayoutType.HORIZONTAL,
-                                                        ListAddress.this.pusher);
-                        ListAddress.this.customer.save();
-                    } catch (SQLException ex) {
-                        ErrorLogger.error(ex, "Failed to delete address record.", true, true);
-                    }
-                    ListAddress.this.fillTable();
+					int selected = ListAddress.this.table.getSelectedRow();
+					if (selected == -1) {
+						return;
+					}
+					
+					int pkey = ListAddress.this.keys.get(selected);
+					try {
+						ListAddress.this.splitPane.push(new AddEditAddress(
+								ListAddress.this.splitPane,
+								ListAddress.this.customer, pkey),
+								SplitPane.LayoutType.HORIZONTAL,
+								ListAddress.this.pusher);
+						ListAddress.this.customer.save();
+					} catch (SQLException ex) {
+						ErrorLogger.error(ex,
+								"Failed to delete address record.", true, true);
+					}
+					ListAddress.this.fillTable();
 				}
 			});
 			c = new GridBagConstraints();
@@ -128,7 +140,7 @@ public class ListAddress extends SimpleListView<Address> {
 			c.weightx = 1;
 			this.add(viewButton, c);
 		}
-
+		
 		c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
 		c.gridy = ++y;
@@ -137,15 +149,15 @@ public class ListAddress extends SimpleListView<Address> {
 		c.weighty = 1;
 		c.weightx = 1;
 		this.add(new JScrollPane(this.table), c);
-
+		
 		this.fillTable();
 	}
-
+	
 	@Override
 	protected DB.TableName getTableName() {
 		return DB.TableName.customer_address;
 	}
-
+	
 	@Override
 	protected ArrayList<Address> mapResults(
 			ArrayList<HashMap<String, Field>> results) {
