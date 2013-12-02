3dd280b915c8f9e4f6820acb25374c03cabb92f3
borgbean <borgbean@sammich.info>
2013-12-01 19:58:09 -0500

    Schema fix
    transaction_item had no qty.

---
+++ src/ISIS/gui/transaction/AddEditTransactionLineItem.java
@@ -0,0 +1,11 @@
+package ISIS.gui.transaction;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelm
+ * Date: 12/1/13
+ * Time: 7:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddEditTransactionLineItem {
+}
