06336bc314b23a59e5fecc2782caa7e4cbe17e04
swoolheater1 <test@Sirang-Woolheaters-MacBook-Pro.local>
2013-12-04 11:52:35 -0500

    HTMLFormatException & StringBuilder Comments

--- src/ISIS/html/HTMLFormatException.java
+++ src/ISIS/html/HTMLFormatException.java
@@ -3,6 +3,9 @@ package ISIS.html;
 public class HTMLFormatException extends Exception {
 	private static final long	serialVersionUID	= 1L;
 	
+        /*
+         * Public constructor: creates HTMLFormatException instance 
+         */
 	public HTMLFormatException(String message) {
 		super(message);
 	}
--- src/ISIS/html/StringBuilder.java
+++ src/ISIS/html/StringBuilder.java
@@ -6,15 +6,26 @@ public class StringBuilder {
 	private java.lang.StringBuilder	b;
 	private int						tabs;
 	
+        /*
+         * Public constructor: new StringBuilder instance
+         */
 	public StringBuilder() {
 		b = new java.lang.StringBuilder();
 		tabs = 0;
 	}
 	
+        /*
+         * @pre - none
+         * @post - append string param to b
+         */
 	public void append(String string) {
 		b.append(string);
 	}
 	
+        /*
+         * @pre - tabs > 0
+         * @post - tabs decremented
+         */
 	public void closeBlock() {
 		if (tabs == 0) {
 			throw new RangeException((short) 1, "Tabs can't be negative");
@@ -22,6 +33,10 @@ public class StringBuilder {
 		tabs--;
 	}
 	
+        /*
+         * @pre - none
+         * @post - returns b
+         */
 	public StringBuilder indents() {
 		for (int i = 0; i < tabs; i++) {
 			b.append("\t");
@@ -29,6 +44,10 @@ public class StringBuilder {
 		return this;
 	}
 	
+        /*
+         * @pre - none
+         * @post - \n is appended to b
+         */
 	public void ln(String string) {
 		String[] strings = string.split("\n");
 		for (String str : strings) {
@@ -38,6 +57,10 @@ public class StringBuilder {
 		}
 	}
 	
+        /*
+         * @pre - none
+         * @post - tabs incremented
+         */
 	public void startBlock() {
 		tabs++;
 	}
