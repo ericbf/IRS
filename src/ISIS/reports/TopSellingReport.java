c625534d9fac15cafb772602c306573b7483538a
erkalicious <erkalicious@icloud.com>
2013-12-06 22:31:02 -0500

    sales report

--- src/ISIS/reports/TopSellingReport.java
+++ src/ISIS/reports/TopSellingReport.java
@@ -20,7 +20,8 @@ import ISIS.session.Session;
  */
 public class TopSellingReport extends Report {
 	ArrayList<Item>			items;
-	HashMap<Item, Float>	grosses;
+	HashMap<Item, Float>	counts;
+	HashMap<Item, Integer>	transactions;
 	
 	/**
 	 * @param title
@@ -29,18 +30,20 @@ public class TopSellingReport extends Report {
 		super("Top Selling Report");
 		
 		this.items = new ArrayList<>();
-		this.grosses = new HashMap<>();
+		this.counts = new HashMap<>();
+		this.transactions = new HashMap<>();
 		
 		try {
 			PreparedStatement st = Session
 					.getDB()
 					.prepareStatement(
-							"SELECT DISTINCT item, count(item) AS cnt FROM transaction_item GROUP BY item ORDER BY cnt DESC;");
+							"SELECT item, sum(cast(quantity AS real)) AS sold_cnt, count(item) AS order_cnt FROM transaction_item GROUP BY item ORDER BY sold_cnt DESC;");
 			ResultSet x = st.executeQuery();
 			int num = 0;
 			while (x.next() && num++ < 10) {
 				Item item = new Item(x.getInt("item"), true);
-				this.grosses.put(item, x.getFloat("cnt"));
+				this.counts.put(item, x.getFloat("sold_cnt"));
+				this.transactions.put(item, x.getInt("order_cnt"));
 				this.items.add(item);
 			}
 		} catch (SQLException e) {
@@ -56,7 +59,7 @@ public class TopSellingReport extends Report {
 	 */
 	@Override
 	public void populateBuilder() {
-		Table t = new Table(3, 0);
+		Table t = new Table(4, 0);
 		Cell[] cells;
 		int x;
 		
@@ -64,15 +67,17 @@ public class TopSellingReport extends Report {
 		t.setHeaderRow(0);
 		cells[x = 0].add("Item Name");
 		cells[++x].add("Price");
-		cells[++x].add("Sales");
+		cells[++x].add("Sold Count");
+		cells[++x].add("Order Count");
 		
 		for (Item item : this.items) {
 			cells = t.addRow();
 			cells[x = 0].add(item.getName());
 			cells[++x].add(String
 					.format("$%,.2f", item.getPrice().floatValue()));
-			cells[++x].add(String.format("%,.2f",
-					(float) this.grosses.get(item)));
+			cells[++x].add(String.format("%,.2f %s",
+					(float) this.counts.get(item), item.getUOM()));
+			cells[++x].add(String.format("%,d", this.transactions.get(item)));
 		}
 		
 		this.b.add(t);
